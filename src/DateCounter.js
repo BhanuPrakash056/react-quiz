9644505e65cc6fe107fa528bf0b4bd4b8a63452c
bhanu prakash <r19cs056@cit.reva.edu.in>
2024-06-08 11:45:33 +0530

    feat: add useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,15 +1,20 @@
 import { useReducer, useState } from "react";
 
 function reducer(state, action) {
-  console.log(state, action);
-  if (action.type === "dec") return state + action.payload;
-  if (action.type === "inc") return state + action.payload;
-  if (action.type === "setCount") return action.payload;
+  console.log(action, state);
+  if (action.type === "inc") {
+    return state + 1;
+  }
+  if (action.type === "dec") {
+    return state - 1;
+  }
+  if (action.type === "setCount") {
+    return action.payload;
+  }
   return state + action;
 }
-
 function DateCounter() {
-  // const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);/
   const [step, setStep] = useState(1);
 
   const [count, dispatch] = useReducer(reducer, 0);
@@ -23,16 +28,15 @@ function DateCounter() {
     // setCount((count) => count - step);
   };
 
-  const inc = function (e) {
+  const inc = function () {
     dispatch({ type: "inc" });
-    // dispatch(1);
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
-    dispatch({ type: "setCount" });
   };
 
   const defineStep = function (e) {
