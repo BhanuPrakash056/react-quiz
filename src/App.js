2d96fd9eec0caabfc3fad73a6cfd919143d63251
bhanu prakash <r19cs056@cit.reva.edu.in>
2024-06-09 09:13:07 +0530

    feat: add reducer and initialState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
-export default function App (){
-  return <div>
-  <DateCounter />
-  </div>
+initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {}
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  });
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>question</p>
+      </Main>
+    </div>
+  );
 }
