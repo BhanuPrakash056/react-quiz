f9c77ad82c0da45fc71eeb290da5cade9908b8e3
bhanu prakash <r19cs056@cit.reva.edu.in>
2024-06-09 10:47:15 +0530

    feat: add the start scree

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
-
-initialState = {
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+const initialState = {
   questions: [],
   status: "loading",
 };
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, questions: [], status: "failed" };
+    default:
+      throw new Error("Unexpected action");
+  }
+}
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
   });
+  const numberOfQuestions = questions.length;
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numberOfQuestions={numberOfQuestions} />
+        )}
       </Main>
     </div>
   );
