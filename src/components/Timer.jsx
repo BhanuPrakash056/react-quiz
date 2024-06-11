d7a298117335ec53f19888eb4d15bd72b63bb677
bhanu prakash <r19cs056@cit.reva.edu.in>
2024-06-11 06:01:32 +0530

    feat: updated the timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 function Timer({ dispatch, secondRemaining }) {
   const mins = Math.floor(secondRemaining / 60);
-  const secondRemaining = Math.floor(secondRemaining / 60);
+  const second = secondRemaining % 60;
   useEffect(
     function () {
       const id = setInterval(function () {
@@ -14,7 +14,11 @@ function Timer({ dispatch, secondRemaining }) {
     [dispatch]
   );
 
-  return <div className="timer">{secondRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"} {mins} : {second < 10 && "0"} {second}
+    </div>
+  );
 }
 
 export default Timer;
